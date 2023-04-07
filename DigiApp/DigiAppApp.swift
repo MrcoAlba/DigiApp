28bb1ed0be997581d8108df4be0c9fe3b6c166fa
mrcoalba <mrcoalba@gmail.com>
2023-04-07 12:50:29 -0500

    Initial Commit

---
+++ DigiApp/DigiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiAppApp.swift
+//  DigiApp
+//
+//  Created by Mrco on 7/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DigiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
